8855f2c6016829504660a565892fba2c4be436d3
Wouter Kettlitz <75383073+Pentadome@users.noreply.github.com>
2021-02-18 14:21:30 +0100

    setup vue

---
+++ pentdadome.intersoftware.codechallenge.vuejs/src/models/Sale.ts
@@ -0,0 +1,6 @@
+export default interface Sale{
+    product : string,
+    price : number,
+    quantity : number,
+    date: Date
+}
---
+++ pentdadome.intersoftware.codechallenge.vuejs/src/views/Graph.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="container" style="width:100%; height:400px;">
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Highcharts from 'highcharts'
+import axios from 'axios'
+import Sale from '@/models/Sale'
+
+@Options({})
+export default class Graph extends Vue {
+  hasBeenLoaded = false;
+  graphData : Sale[] = [];
+  
+}
+</script>
