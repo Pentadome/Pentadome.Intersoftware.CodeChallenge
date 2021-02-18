a8edd75f3eb8ce1a7ada151d40efbe98cfdbb27a
Wouter Kettlitz <75383073+Pentadome@users.noreply.github.com>
2021-02-18 17:14:44 +0100

    finished quantity sold

--- pentdadome.intersoftware.codechallenge.vuejs/src/models/Sale.ts
+++ pentdadome.intersoftware.codechallenge.vuejs/src/models/Sale.ts
@@ -1,6 +1,6 @@
 export default interface Sale{
-    product : string,
-    price : number,
-    quantity : number,
-    date: Date
+    product: string;
+    price: number;
+    quantity: number;
+    date: Date;
 }
--- pentdadome.intersoftware.codechallenge.vuejs/src/views/Graph.vue
+++ pentdadome.intersoftware.codechallenge.vuejs/src/views/Graph.vue
@@ -1,19 +1,42 @@
 <template>
   <div id="container" style="width:100%; height:400px;">
-    
+
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import Highcharts from 'highcharts'
+import ProductMonthlySaleRecord from '@/models/ProductMonthlySaleRecord'
+import Highcharts, { Axis, Chart, SeriesLineOptions, SeriesOptionsType } from 'highcharts'
 import axios from 'axios'
 import Sale from '@/models/Sale'
 
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 @Options({})
 export default class Graph extends Vue {
-  hasBeenLoaded = false;
-  graphData : Sale[] = [];
-  
+  chart: Highcharts.Chart|null = null;
+  created () {
+    axios.get<ProductMonthlySaleRecord[]>('https://localhost:44393/api/sales/GetSalesByMonth').then(x => {
+      const graphData = x.data.map(x => ({ name: x.product, data: x.saleRecords.map(y => y.quantity), type: 'line' })) as SeriesLineOptions[];
+      this.chart = Highcharts.chart({
+        chart: {
+          renderTo: 'container'
+        },
+        title: {
+          text: 'Product Sales By month'
+        },
+        xAxis: {
+          categories: months
+        },
+        yAxis: {
+          title: {
+            text: 'Amount Sold'
+          }
+        },
+        series: graphData
+      })
+    })
+  };
 }
 </script>
