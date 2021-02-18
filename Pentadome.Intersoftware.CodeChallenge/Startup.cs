279a4fb41a4897968cf8f4aecf813060d96a831e
Wouter Kettlitz <75383073+Pentadome@users.noreply.github.com>
2021-02-18 14:52:17 +0100

    preparedApiForGraph

--- Pentadome.Intersoftware.CodeChallenge/Startup.cs
+++ Pentadome.Intersoftware.CodeChallenge/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Pentadome.Intersoftware.CodeChallenge.Data.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,6 +28,8 @@ namespace Pentadome.Intersoftware.CodeChallenge
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSingleton<CsvDataRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
