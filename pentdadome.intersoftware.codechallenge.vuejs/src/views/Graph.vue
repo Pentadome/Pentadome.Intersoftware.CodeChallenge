1fe20e2f8b213b3797bbd0bd340312e17940bd5e
Wouter Kettlitz <75383073+Pentadome@users.noreply.github.com>
2021-02-19 10:33:46 +0100

    added turnover graph

--- pentdadome.intersoftware.codechallenge.vuejs/src/views/Graph.vue
+++ pentdadome.intersoftware.codechallenge.vuejs/src/views/Graph.vue
@@ -2,6 +2,9 @@
   <div id="container" style="width:100%; height:400px;">
 
   </div>
+    <div id="container2" style="width:100%; height:400px;">
+
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,14 +14,16 @@ import Highcharts, { Axis, Chart, SeriesLineOptions, SeriesOptionsType } from 'h
 import axios from 'axios'
 import Sale from '@/models/Sale'
 
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 @Options({})
 export default class Graph extends Vue {
   chart: Highcharts.Chart|null = null;
+  chartTurnover: Highcharts.Chart|null = null;
+
   created () {
     axios.get<ProductMonthlySaleRecord[]>('https://localhost:44393/api/sales/GetSalesByMonth').then(x => {
-      const graphData = x.data.map(x => ({ name: x.product, data: x.saleRecords.map(y => y.quantity), type: 'line' })) as SeriesLineOptions[];
+      const graphDataAmountSold = x.data.map(x => ({ name: x.product, data: x.saleRecords.map(y => y.quantity), type: 'line' })) as SeriesLineOptions[]
       this.chart = Highcharts.chart({
         chart: {
           renderTo: 'container'
@@ -34,7 +39,25 @@ export default class Graph extends Vue {
             text: 'Amount Sold'
           }
         },
-        series: graphData
+        series: graphDataAmountSold
+      })
+      const graphDataTurnover = x.data.map(x => ({ name: x.product, data: x.saleRecords.map(y => y.turnOver), type: 'line' })) as SeriesLineOptions[]
+      this.chartTurnover = Highcharts.chart({
+        chart: {
+          renderTo: 'container2'
+        },
+        title: {
+          text: 'Turnover of Product By month'
+        },
+        xAxis: {
+          categories: months
+        },
+        yAxis: {
+          title: {
+            text: 'Amount in Euros'
+          }
+        },
+        series: graphDataTurnover
       })
     })
   };
